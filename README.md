7bb68645022a292c5f29ac84c593c37759e72828
denis-fominykh <fominykhdenis@gmail.com>
2020-05-27 23:27:42 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# CSS Theme Switcher
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+# or
+yarn install
+```
+
+To run:
+
+```sh
+npm run start
+# or
+yarn start
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+# or
+yarn build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+# or
+yarn build-dev
+```
+
+## Running
+
+```sh
+node dist/bundle.js
+```
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  entry: ['./src/index.ts', './src/scss/styles.scss'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x)?$/,
+        use: ['awesome-typescript-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: 'app',
+      filename: 'index.html',
+      template: 'src/html/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
+
+module.exports = config;
